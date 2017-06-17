f8cfd14d291716edf2832e9c461e5840d73f25ec
ouerqiang <ouerqiang@baidu.com>
2017-06-17 11:11:34 +0800

    add doc

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
 #beegofreecache
+## 什么是beegofreecache
+```go
+fatal error: concurrent map iteration and map write
+```
+更重要的是memory使用的是同步锁来确保数据一致性，这必然后导致性能有所损耗。
+如果有熟悉Java的同学应该知道Map。HashMap中未进行同步考虑，而Hashtable则使用了synchronized，带来的直接影响就是可选择，我们可以在单线程时使用HashMap提高效率，而多线程时用Hashtable来保证安全。
+beego内置的memory实现就相当于Hashtable。  
+所以我们需要一个更好的memory的实现，经过测试决定使用freecache，大家可以去[freecache主页](https://github.com/coocood/freecache)查看详细信息。
+freecache就相当于Java中的ConcurrentHashMap，性能也有很大的提高，官网上它的性能测试报告，在此不赘述了。  
+
+但是freecache也会有一些缺点：
+1. 当需要缓存的数据占用超过提前分配缓存的 1/1024 则不能缓存
+2. 当分配内存过多则会导致内存占用高
+3. key的长度需要小于65535字节
+3. value只能是[]byte类型，使用很不友好
+## 使用方法
+为此我对freecache进行了简单的封装，让freecache可以集成搭配beego框架中，也可以不在beego框架中独立使用。  
+### 集成到beego框架:
+与beego使用其他的cache一样。
+```go
+var bm, err = cache.NewCache("beegofreecache", `{"size":64}`)
+bm.Put("beegofreecache", 1, 10*time.Second)
+bm.Get("beegofreecache")
+bm.IsExist("beegofreecache")
+bm.Delete("beegofreecache")
+```
+**注意：Get()函数返回的是[]byte这个需要业务使用beegofreecache内置GobDecode()转码**
+```go
+var bm, err = cache.NewCache("beegofreecache", `{"size":64}`)
+func TestBeeFreeCacheGP(t *testing.T)  {
+
+    if err!=nil {
+        println(err.Error())
+    }
+    key:="key"
+    bm.Put(key,1200,10*time.Second)
+    val:=bm.Get(key)
+    var ii int
+    GobDecode(val.([]byte),&ii)
+    println(val,ii)
+
+}
+```
+### 独立使用
+不依赖beego框架。  
+```go
+bm:=beegofreecache.NewFree(512)//size为M
+bm.Put("beegofreecache", 1, 10*time.Second)
+var ii int
+bm.GetValue("beegofreecache"，ii)
+bm.IsExist("beegofreecache")
+bm.Delete("beegofreecache")
+```  
+欢迎大家提供修改意见。
--- beegofreecache_test.go
+++ beegofreecache_test.go
@@ -5,19 +5,107 @@ import (
     "github.com/astaxie/beego/cache"
     "time"
     . "beegofreecache"
+    "strconv"
 )
 
+var bm, err = cache.NewCache("beegofreecache", `{"size":64}`)
+func TestBeeFreeCacheGP(t *testing.T)  {
 
-func TestBeeFreeCache(t *testing.T)  {
-    var bm, err = cache.NewCache("beegofreecache", `{"size":64}`)
     if err!=nil {
         println(err.Error())
     }
-
-    bm.Put("key",1200,10*time.Second)
-    val:=bm.Get("key")
+    key:="key"
+    bm.Put(key,1200,10*time.Second)
+    val:=bm.Get(key)
     var ii int
     GobDecode(val.([]byte),&ii)
     println(val,ii)
 
+}
+
+func TestBeeFreeCacheDel(t *testing.T)  {
+    //var bm, err = cache.NewCache("beegofreecache", `{"size":64}`)
+    if err!=nil {
+        println(err.Error())
+    }
+    key:="key"
+    bm.Put(key,1200,10*time.Second)
+    val:=bm.Get(key)
+    var ii int
+    GobDecode(val.([]byte),&ii)
+    println(val,ii)
+    bm.Delete(key)
+    val=bm.Get(key)
+    println(val)
+}
+
+func TestBeeFreeCacheGetMu(t *testing.T)  {
+    //var bm, err = cache.NewCache("beegofreecache", `{"size":64}`)
+    if err!=nil {
+        println(err.Error())
+    }
+    key:="key"
+    for i:=0;i<10 ;i++  {
+        bm.Put(key+strconv.Itoa(i),i*23,10*time.Second)
+    }
+
+    vals:=bm.GetMulti([]string{"key0","key1","key2","key3"})
+    for _,val:=range vals{
+        var ii int
+        GobDecode(val.([]byte),&ii)
+        println(val,ii)
+    }
+}
+
+func TestBeeFreeCacheCr(t *testing.T)  {
+   // var bm, err = cache.NewCache("beegofreecache", `{"size":64}`)
+    if err!=nil {
+        println(err.Error())
+    }
+    key:="key"
+    bm.Put(key,int64(0),10*time.Second)
+
+    val:=bm.Get(key)
+    var ii int64
+    GobDecode(val.([]byte),&ii)
+    println("get",val,ii)
+
+    err=bm.Incr(key)
+    val=bm.Get(key)
+    GobDecode(val.([]byte),&ii)
+    println("Incr",err,val,ii)
+    bm.Decr(key)
+    val=bm.Get(key)
+    GobDecode(val.([]byte),&ii)
+    println(val,ii)
+}
+
+func TestBeeFreeCacheEx(t *testing.T)  {
+    key:="exist"
+    bm.Put(key,"TestBeeFreeCacheEx",10*time.Second)
+    b:=bm.IsExist(key)
+    if b{
+        val:=bm.Get(key)
+        var ii string
+        GobDecode(val.([]byte),&ii)
+        println("get",val,ii)
+    }
+    b=bm.IsExist("noexist")
+    println("noexist",b)
+}
+
+func TestBeeFreeCacheClear(t *testing.T)  {
+
+    key:="key"
+    for i:=0;i<10 ;i++  {
+        bm.Put(key+strconv.Itoa(i),i*23,10*time.Second)
+    }
+    for i:=0;i<10 ;i++  {
+        println(key+strconv.Itoa(i),bm.IsExist(key+strconv.Itoa(i)))
+    }
+    bm.ClearAll()
+
+    for i:=0;i<10 ;i++  {
+        println(key+strconv.Itoa(i),bm.IsExist(key+strconv.Itoa(i)))
+    }
 }
